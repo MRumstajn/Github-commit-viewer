2ea27e29a52a328a2115b2bed7d7608605939763
MRumstajn <mrumstajn@student.unizd.hr>
2022-12-07 20:31:55 +0100

    added commit details activity

---
+++ app/src/main/java/com/mauricio/githubcommitviewer/commit_details/CommitDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.mauricio.githubcommitviewer.commit_details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mauricio.githubcommitviewer.R;
+
+public class CommitDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commit_details);
+    }
+}
